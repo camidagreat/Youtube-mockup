b5faa6935c917a84da5716416fd53aa01618ea38
camidagreat <lbidigarecurtis@gmail.com>
2017-12-06 09:30:21 -0500

    starting on the youtube video api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCSVJk2swAR10hDhWXnuh8kCmrLkuDnty4'
+// Create new component and show some HTML
+// some HTML
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = function() {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+// take component generated HTML and
+// put it on the page (in DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
