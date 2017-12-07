ab0e4232988a5b8e256af7186d06e675a5a48282
camidagreat <lbidigarecurtis@gmail.com>
2017-12-07 18:12:41 -0500

    Have got most of the components in place, the video plays, shows 5 top videos hits, youtube api working

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyCSVJk2swAR10hDhWXnuh8kCmrLkuDnty4'
-// Create new component and show some HTML
-// some HTML
+const API_KEY = 'AIzaSyCSVJk2swAR10hDhWXnuh8kCmrLkuDnty4';
 
-const App = function() {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'grace'}, (videos) => {
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video= {this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect= {selectedVideo=this.setState({selectedVideo}) }
+          videos= {this.state.videos} />
+      </div>
+    );
+  }
 }
-
-// take component generated HTML and
-// put it on the page (in DOM)
-
 ReactDOM.render(<App />, document.querySelector('.container'));
